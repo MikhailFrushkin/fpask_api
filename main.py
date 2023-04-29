3ab40789380bd668f5e7d7ae2369da08668949c6
Mikhail Fryshkin <muxazila@mail.ru>
2023-04-30 00:07:15 +0700

    122

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+import json
+
+import requests
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
-from flask import Flask, request
-from loguru import logger
-
 from environs import Env
+from flask import Flask, request, jsonify
+from loguru import logger
 
 env = Env()
 env.read_env()
 
 application = Flask(__name__)
+application.debug = True
 
 bot_token_1 = env.str('bot_token_1')
 bot_token_2 = env.str('bot_token_2')
@@ -17,61 +20,58 @@ bot_2 = Bot(token=bot_token_2)
 dp_1 = Dispatcher(bot_1)
 dp_2 = Dispatcher(bot_2)
 
-WEBHOOK_HOST = "https://mikhailfryshkin.online"
-WEBHOOK_PATH = f"/{bot_token_1}"
-WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+URL1 = "https://api.telegram.org/bot{}/".format(bot_token_1)
+URL2 = "https://api.telegram.org/bot{}/".format(bot_token_2)
+
+webhook1 = 'https://api.telegram.org/bot6087228959:AAExcei7wDfKznQEv6jrkAKlTzSIx9Dpu_E/setWebhook?url=https://mikhailfryshkin.ru'
+webhook_del = 'https://api.telegram.org/bot6087228959:AAExcei7wDfKznQEv6jrkAKlTzSIx9Dpu_E/deleteWebhook'
 
 
-@application.route("/")
+def write_json(data, file_name='answer.json'):
+    with open(file_name, 'w') as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+
+
+@application.route("/", methods=['POST', 'GET'])
 async def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    if request.method == 'POST':
+        try:
+            data = request.get_json()
+            with open('asdasd.txt', 'w') as f:
+                f.write(data)
+            logger.info(f"{data}\n")
+
+            chat_id = data['message']['chat']['id']
+            message_text = data['message']['text']
+
+            # Отправляем запрос на поиск товара второму боту
+            search_result = search_product(message_text, chat_id)
+
+            # Отправляем результаты поиска в чат
+            send_message(chat_id, search_result)
+            return jsonify(data)
+        except Exception as ex:
+            logger.debug(ex)
+    return f"<h1 style='color:blue'>{request.method}</h1>"
 
 
-@dp_1.message_handler()
-async def handle_message_from_bot_1(message: types.Message):
-    await bot_2.send_message(chat_id=message.chat.id, text=message.text)
+def search_product(product_code, chat_id):
+    try:
+        url = URL2 + "sendMessage?text={}&chat_id={}".format(product_code, chat_id)
+        response = requests.get(url)
+        logger.info(f"{url}\n{response.text}")
+        search_result = json.loads(response.content)['result']
+        write_json(search_result.json(), 'qwe.json')
+        return search_result
+    except Exception as ex:
+        logger.debug(ex)
 
 
-@dp_2.message_handler()
-async def handle_message_from_bot_2(message: types.Message):
-    await bot_1.send_message(chat_id=message.chat.id, text=message.text)
-
-
-async def on_startup(dp):
-    await bot_1.set_webhook(WEBHOOK_URL)
-
-
-async def on_shutdown(dp):
-    logger.warning('Shutting down..')
-    await bot_1.delete_webhook()
-    await dp.storage.close()
-    await dp.storage.wait_closed()
-
-    logger.warning('Bye!')
-
-
-@application.route(WEBHOOK_PATH, methods=['POST'])
-async def telegram_webhook_1():
-    data = await request.get_data()
-    update = types.Update.to_object(data)
-    print(data)
-    await dp_1.process_updates([update])
-    return '', 200
-
-
-@application.route(f'/{bot_token_2}', methods=['POST'])
-async def telegram_webhook_2():
-    data = await request.get_data()
-    update = types.Update.to_object(data)
-    await dp_2.process_updates([update])
-    return '', 200
+def send_message(chat_id, text):
+    url = URL1 + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+    requests.get(url)
 
 
 if __name__ == '__main__':
-    executor.start_webhook(dispatcher=dp_1,
-                           webhook_path=WEBHOOK_PATH,
-                           on_startup=on_startup,
-                           on_shutdown=on_shutdown,
-                           skip_updates=True, )
-    # executor.start_webhook(dispatcher=dp_2, webhook_path=f'/{bot_token_2}', host=bot_url_2)
+    logger.add('info.log', format='{time} {level} {message}')
     application.run(host='0.0.0.0')
